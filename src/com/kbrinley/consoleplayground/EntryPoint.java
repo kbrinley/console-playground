d8e24ba8a0b672fc3c627205d5f7747def6cdd19
Kevin Brinley <kbrinley@gmail.com>
2017-08-19 16:42:30 -0400

    Add EntryPoint class, i.e., the main class

---
+++ src/com/kbrinley/consoleplayground/EntryPoint.java
@@ -0,0 +1,12 @@
+package com.kbrinley.consoleplayground;
+
+public class EntryPoint {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Java!");
+	}
+
+}
